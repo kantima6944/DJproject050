708e909d822732cfdc2fb8a8fd3fe39498353e62
kantima6944 <kantima.yu@rmuti.ac.th>
2023-02-12 21:38:30 +0700

    third1 6 กพ

--- ProfileApp/forms.py
+++ ProfileApp/forms.py
@@ -10,3 +10,107 @@ class ProductFrom(forms.Form):
     price = forms.IntegerField(label="ราคา")
     amount = forms.IntegerField(min_value=0, label="คงเหลือ")
     promotion = forms.CharField(label= "โปรโมชั่น",widget=forms.Select(choices= choicePro))
+
+class GoodsForm(forms.ModelForm):
+     class Meta:
+        model = Goods
+        fields = ('g_category', 'gid', 'g_name', 'brand', 'price', 'Net', 'product')
+        widgets = {
+                'g_category': forms.Select(attrs={'class': 'form-control'}),
+                'gid': forms.TextInput(attrs={'class': 'form-control'}),
+                'g_name': forms.TextInput(attrs={'class': 'form-control'}),
+                'brand': forms.TextInput(attrs={'class': 'form-control'}),
+                'g_models': forms.TextInput(attrs={'class': 'form-control'}),
+                'price': forms.NumberInput(attrs={'class': 'form-control', 'min': 1}),
+                'Net': forms.NumberInput(attrs={'class': 'form-control', 'min': 0}),
+                'product': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'cols': 60}),
+            }
+        labels = {
+                'g_category': 'Goodscategory',
+                'gid': 'Goodsid',
+                'g_name': 'Goodsname',
+                'brand': 'Brand',
+                'g_models': 'Models',
+                'price': 'Price',
+                'net': 'Net',
+                'product': 'Property',
+
+        }
+
+class GoodsFormUpdate(forms.ModelForm):
+    class Meta:
+            model = Goods
+            fields = ('g_category', 'gid', 'g_name', 'brand', 'price', 'Net', 'product')
+            widgets = {
+                'g_category': forms.Select(attrs={'class': 'form-control'}),
+                'gid': forms.TextInput(attrs={'class': 'form-control', 'readonly': 'readonly'}),
+                'g_name': forms.TextInput(attrs={'class': 'form-control'}),
+                'brand': forms.TextInput(attrs={'class': 'form-control'}),
+                'g_models': forms.TextInput(attrs={'class': 'form-control'}),
+                'price': forms.NumberInput(attrs={'class': 'form-control', 'min': 1}),
+                'Net': forms.NumberInput(attrs={'class': 'form-control', 'min': 0}),
+                'product': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'cols': 60}),
+            }
+            labels = {
+                'g_category': 'Goodscategory',
+                'gid': 'Goodsid',
+                'g_name': 'Goodsname',
+                'brand': 'Brand',
+                'g_models': 'Models',
+                'price': 'Price',
+                'net': 'Net',
+                'product': 'Property',
+
+            }
+
+class CusForm(forms.ModelForm):
+        class Meta:
+            model = Customer
+            fields = ('cid', 'name', 'surname', 'address', 'tel', 'gender', 'carreer', 'password')
+            CHOICES = [('M', 'Male'), ('F', 'Female')]
+            widgets = {
+                'cid': forms.TextInput(attrs={'class': 'form-control'}),
+                'name': forms.TextInput(attrs={'class': 'form-control'}),
+                'surname': forms.TextInput(attrs={'class': 'form-control'}),
+                'address': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'cols': 60}),
+                'tel': forms.TextInput(attrs={'class': 'form-control'}),
+                'gender': forms.RadioSelect(choices=CHOICES),
+                'carreer': forms.TextInput(attrs={'class': 'form-control'}),
+                'password': forms.PasswordInput(attrs={'class': 'form-control'}),
+            }
+            labels = {
+                'cid': 'Cid',
+                'name': 'name',
+                'surname': 'surname',
+                'address': 'address',
+                'tel': 'Tel',
+                'gender': 'gender',
+                'carreer': 'carreer',
+                'password': 'password'
+            }
+
+class CusFormUpdate(forms.ModelForm):
+        class Meta:
+            model = Customer
+            fields = ('cid', 'name', 'surname', 'address', 'tel', 'gender', 'carreer', 'password')
+            CHOICES = [('M', 'Male'), ('F', 'Female')]
+            widgets = {
+                'cid': forms.TextInput(attrs={'class': 'form-control', 'readonly': 'readonly'}),
+                'name': forms.TextInput(attrs={'class': 'form-control'}),
+                'surname': forms.TextInput(attrs={'class': 'form-control'}),
+                'address': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'cols': 60}),
+                'tel': forms.TextInput(attrs={'class': 'form-control'}),
+                'gender': forms.RadioSelect(choices=CHOICES),
+                'carreer': forms.TextInput(attrs={'class': 'form-control'}),
+                'password': forms.PasswordInput(attrs={'class': 'form-control'}),
+            }
+            labels = {
+                'cid': 'Cid',
+                'name': 'name',
+                'surname': 'surname',
+                'address': 'address',
+                'tel': 'Tel',
+                'gender': 'gender',
+                'carreer': 'carreer',
+                'password': 'password'
+            }
